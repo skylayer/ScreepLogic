42889405f02da271ab7f0870274b9b954171c835
skylayer <chenjsh@shanghaitech.edu.cn>
2023-09-17 17:44:10 +0800

    [Feat] Simplify generate logic

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
-let cfg;
-const dest = process.env.DEST;
-if (!dest) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
-  throw new Error("Invalid upload destination");
-}
-
 export default {
   input: "src/main.ts",
   output: {
