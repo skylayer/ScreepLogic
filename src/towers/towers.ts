582148b23437f71f1bef61c6dcb7ecb1f63c29d5
skylayer <chenjsh@shanghaitech.edu.cn>
2023-09-21 00:21:07 +0800

    Build on 2023-09-21 00:21:07

--- src/towers/towers.ts
+++ src/towers/towers.ts
@@ -6,6 +6,14 @@ export function controlTowers(room: Room) {
 
   // For each tower, decide what action to take
   for (const tower of towers) {
+
+    // 4. Attack: Find the closest enemy
+    const closestEnemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestEnemy) {
+      tower.attack(closestEnemy);
+      continue;  // Move to next tower
+    }
+
     // 3. Heal: Find the closest hurt creep
     const closestHurtCreep = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
       filter: (creep) => creep.hits < creep.hitsMax
@@ -15,33 +23,21 @@ export function controlTowers(room: Room) {
       continue;  // Move to next tower
     }
 
-    // 4. Attack: Find the closest enemy
-    const closestEnemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (closestEnemy) {
-      tower.attack(closestEnemy);
-      continue;  // Move to next tower
-    }
-
-    // 1. Prioritize repair for decaying structures that are about to break
-    const criticalDecayingStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-      filter: (structure) => {
-        return (structure.structureType === STRUCTURE_ROAD || structure.structureType === STRUCTURE_RAMPART || structure.structureType === STRUCTURE_CONTAINER) &&
-          structure.hits < structure.hitsMax &&
-          structure.hits < 3000;  // or whatever threshold you deem "critical"
-      }
-    });
-    if (criticalDecayingStructure) {
-      tower.repair(criticalDecayingStructure);
-      continue;  // Move to next tower
-    }
-
     // 2. Repair: Find the closest damaged structure
-    const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    const damagedStructure = tower.room.find(FIND_STRUCTURES, {
       filter: (structure) => structure.hits < structure.hitsMax && structure.structureType !== STRUCTURE_WALL
     });
-    if (closestDamagedStructure) {
-      tower.repair(closestDamagedStructure);
-      continue;  // Move to next tower
+    let lowestHit = Infinity;
+    let lowestStructure: AnyStructure = damagedStructure[0];
+    for (const index in damagedStructure) {
+      const struct = damagedStructure[index]
+      if (struct.hits < lowestHit) {
+        lowestStructure = struct
+        lowestHit = struct.hits
+      }
+    }
+    if (!lowestStructure) {
+      tower.repair(lowestStructure)
     }
   }
 }
